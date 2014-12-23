5754431e4e60ac16f3486a0f356ac6bc4f29701b
Casey Klimkowsky <cklimkowsky@gmail.com>
2014-12-23 13:04:22 -0800

    Script now moves and renames APKs when downloaded

--- android_script.sh
+++ android_script.sh
@@ -2,5 +2,9 @@
 while read package_name
 do
 	( cd android_scraper_2 ; scrapy crawl apkspider -a package_name=$package_name )
-	( cd google-play-crawler/googleplay ; java -jar googleplaycrawler-0.3.jar -f crawler.conf download $package_name)
+	( cd google-play-crawler/googleplay ; java -jar googleplaycrawler-0.3.jar -f crawler.conf download $package_name )
+
+	today=$(date +"%m-%d-%Y")
+	mkdir -p downloads
+	( mv google-play-crawler/googleplay/$package_name.apk downloads/${package_name}_${today}.apk )
 done < $1
