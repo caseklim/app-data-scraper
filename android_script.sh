52e923e991f89816f9d5fd4353b1e033c40b3415
Casey Klimkowsky <cklimkowsky@gmail.com>
2014-12-23 15:06:58 -0600

    Added comments to script

--- android_script.sh
+++ android_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 while read package_name
 do
+	# Collect info on the app from Google Play, and then download the respective APK
 	( cd android_scraper_2 ; scrapy crawl apkspider -a package_name=$package_name )
 	( cd google-play-crawler/googleplay ; java -jar googleplaycrawler-0.3.jar -f crawler.conf download $package_name )
 
+	# Once the APK is downloaded, move and rename the file
 	today=$(date +"%m-%d-%Y")
 	mkdir -p downloads
-	( mv google-play-crawler/googleplay/$package_name.apk downloads/${package_name}_${today}.apk )
+	mv google-play-crawler/googleplay/$package_name.apk downloads/${package_name}_${today}.apk
 done < $1
