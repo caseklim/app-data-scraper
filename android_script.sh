7b5eeb458d0919fa091c49e51ec52265aee49757
Casey Klimkowsky <cklimkowsky@gmail.com>
2015-02-05 13:46:56 -0500

    Decided to keep google-play-crawler as-is... renamed again

--- android_script.sh
+++ android_script.sh
@@ -4,10 +4,10 @@ while read package_name
 do
 	# Collect info on the app from Google Play, and then download the respective APK
 	( cd android_scraper ; scrapy crawl apk -a package_name="$package_name" -a start_time="$start_time" )
-	( cd google_play_crawler/googleplay ; java -jar googleplaycrawler-0.3.jar -f crawler.conf download $package_name )
+	( cd google-play-crawler/googleplay ; java -jar googleplaycrawler-0.3.jar -f crawler.conf download $package_name )
 
 	# Once the APK is downloaded, move and rename the file
 	today=$( date +"%m-%d-%Y" )
 	mkdir -p downloads
-	mv google_play_crawler/googleplay/$package_name.apk downloads/${package_name}_${today}.apk
+	mv google-play-crawler/googleplay/$package_name.apk downloads/${package_name}_${today}.apk
 done < $1
