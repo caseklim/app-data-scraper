6f2f45eb2442eefddcb98ee7007004533c91a2e2
Casey Klimkowsky <cklimkowsky@gmail.com>
2015-03-15 16:07:41 -0400

    Modified android_script-2.sh to download APK files directly into /Android

--- android_scraper/android_script-2.sh
+++ android_scraper/android_script-2.sh
@@ -2,4 +2,4 @@
 
 # Once the APK is downloaded, move and rename the file to the following format: package.name-YYY_MM_DD.apk where YYYY_MM_DD is the date on which it was updated
 mkdir -p downloads
-mv ../google-play-crawler/googleplay/$1.apk downloads/${1}-${2}_${3}_${4}.apk
+mv ../google-play-crawler/googleplay/$1.apk ../../Android/${1}-${2}_${3}_${4}.apk
